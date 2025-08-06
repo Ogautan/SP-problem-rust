461d70543a4c2ef4415019828e0cd4ce97a0de28
Ogautan <yj56755508@163.com>
2025-08-06 09:37:37 +0800

    move cache into args

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
 mod solution {
     use std::collections::{HashMap, HashSet};
-    use std::sync::Mutex;
 
     const MIN: i32 = 2;
     const MAX: i32 = 99;
-
-    use once_cell::sync::Lazy;
-
-    static SUM_PAIRS_CACHE: Lazy<Mutex<HashMap<i32, Vec<(i32, i32)>>>> =
-        Lazy::new(|| Mutex::new(HashMap::new()));
-    static PRODUCT_PAIRS_CACHE: Lazy<Mutex<HashMap<i32, Vec<(i32, i32)>>>> =
-        Lazy::new(|| Mutex::new(HashMap::new()));
-    static PRODUCT_NUMBER_CACHE: Lazy<Mutex<HashMap<i32, bool>>> =
-        Lazy::new(|| Mutex::new(HashMap::new()));
 
     fn get_all_pairs() -> HashSet<(i32, i32)> {
         let mut pairs = HashSet::new();
@@ -24,9 +14,11 @@ mod solution {
         pairs
     }
 
-    fn get_sum_pairs(n: i32) -> Vec<(i32, i32)> {
-        let mut cache = SUM_PAIRS_CACHE.lock().unwrap();
-        if let Some(v) = cache.get(&n) {
+    fn get_sum_pairs(
+        n: i32,
+        sum_pairs_cache: &mut HashMap<i32, Vec<(i32, i32)>>,
+    ) -> Vec<(i32, i32)> {
+        if let Some(v) = sum_pairs_cache.get(&n) {
             return v.clone();
         }
         let half_n = n / 2;
@@ -34,13 +26,15 @@ mod solution {
             .filter(|i| (n - i) <= MAX)
             .map(|i| (i, n - i))
             .collect();
-        cache.insert(n, v.clone());
+        sum_pairs_cache.insert(n, v.clone());
         v
     }
 
-    fn get_product_pairs(n: i32) -> Vec<(i32, i32)> {
-        let mut cache = PRODUCT_PAIRS_CACHE.lock().unwrap();
-        if let Some(v) = cache.get(&n) {
+    fn get_product_pairs(
+        n: i32,
+        product_pairs_cache: &mut HashMap<i32, Vec<(i32, i32)>>,
+    ) -> Vec<(i32, i32)> {
+        if let Some(v) = product_pairs_cache.get(&n) {
             return v.clone();
         }
         let sqrt_n = (n as f64).sqrt() as i32;
@@ -48,28 +42,38 @@ mod solution {
             .filter(|i| n % i == 0)
             .map(|i| (i, n / i))
             .collect();
-        cache.insert(n, v.clone());
+        product_pairs_cache.insert(n, v.clone());
         v
     }
 
-    fn can_be_product_number(n: i32) -> bool {
-        let mut cache = PRODUCT_NUMBER_CACHE.lock().unwrap();
-        if let Some(&b) = cache.get(&n) {
+    fn can_be_product_number(n: i32, product_number_cache: &mut HashMap<i32, bool>) -> bool {
+        if let Some(&b) = product_number_cache.get(&n) {
             return b;
         }
         let sqrt_n = (n as f64).sqrt() as i32;
         let b = (MIN..=sqrt_n).filter(|i| n % i == 0).take(2).count() == 2;
-        cache.insert(n, b);
+        product_number_cache.insert(n, b);
         b
     }
 
-    fn satisfy_s1((i, j): (i32, i32)) -> bool {
-        let sum_pairs = get_sum_pairs(i + j);
-        sum_pairs.len() >= 2 && sum_pairs.iter().all(|(x, y)| can_be_product_number(x * y))
+    fn satisfy_s1(
+        (i, j): (i32, i32),
+        sum_pairs_cache: &mut HashMap<i32, Vec<(i32, i32)>>,
+        product_number_cache: &mut HashMap<i32, bool>,
+    ) -> bool {
+        let sum_pairs = get_sum_pairs(i + j, sum_pairs_cache);
+        sum_pairs.len() >= 2
+            && sum_pairs
+                .iter()
+                .all(|(x, y)| can_be_product_number(x * y, product_number_cache))
     }
 
-    fn satisfy_s2((i, j): (i32, i32), s1: &HashSet<(i32, i32)>) -> bool {
-        let product_pairs = get_product_pairs(i * j);
+    fn satisfy_s2(
+        (i, j): (i32, i32),
+        s1: &HashSet<(i32, i32)>,
+        product_pairs_cache: &mut HashMap<i32, Vec<(i32, i32)>>,
+    ) -> bool {
+        let product_pairs = get_product_pairs(i * j, product_pairs_cache);
         product_pairs
             .iter()
             .filter(|&&(x, y)| s1.contains(&(x, y)))
@@ -78,8 +82,12 @@ mod solution {
             == 1
     }
 
-    fn satisfy_s3((i, j): (i32, i32), s2: &HashSet<(i32, i32)>) -> bool {
-        let sum_pairs = get_sum_pairs(i + j);
+    fn satisfy_s3(
+        (i, j): (i32, i32),
+        s2: &HashSet<(i32, i32)>,
+        sum_pairs_cache: &mut HashMap<i32, Vec<(i32, i32)>>,
+    ) -> bool {
+        let sum_pairs = get_sum_pairs(i + j, sum_pairs_cache);
         sum_pairs
             .iter()
             .filter(|&&(x, y)| s2.contains(&(x, y)))
@@ -88,9 +96,9 @@ mod solution {
             == 1
     }
 
-    fn sieve<F>(set: &HashSet<(i32, i32)>, predicate: F) -> HashSet<(i32, i32)>
+    fn sieve<F>(set: &HashSet<(i32, i32)>, mut predicate: F) -> HashSet<(i32, i32)>
     where
-        F: Fn((i32, i32)) -> bool,
+        F: FnMut((i32, i32)) -> bool,
     {
         set.iter()
             .filter(|&&(i, j)| predicate((i, j)))
@@ -99,10 +107,16 @@ mod solution {
     }
 
     pub fn solve() -> HashSet<(i32, i32)> {
+        let mut sum_pairs_cache = HashMap::new();
+        let mut product_pairs_cache = HashMap::new();
+        let mut product_number_cache = HashMap::new();
+
         let all_pairs = get_all_pairs();
-        let s1 = sieve(&all_pairs, satisfy_s1);
-        let s2 = sieve(&s1, |(i, j)| satisfy_s2((i, j), &s1));
-        let s3 = sieve(&s2, |(i, j)| satisfy_s3((i, j), &s2));
+        let s1 = sieve(&all_pairs, |pair| {
+            satisfy_s1(pair, &mut sum_pairs_cache, &mut product_number_cache)
+        });
+        let s2 = sieve(&s1, |pair| satisfy_s2(pair, &s1, &mut product_pairs_cache));
+        let s3 = sieve(&s2, |pair| satisfy_s3(pair, &s2, &mut sum_pairs_cache));
         s3
     }
 }
